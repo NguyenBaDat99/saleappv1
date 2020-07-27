d42b0a94a4aa8b5b7dbeaf3c6f6e47d2af370ad5
nguyenbadat99 <nguyenbadat3107@gmail.com>
2020-07-27 11:20:05 +0700

    Update code 2707

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -11,9 +11,54 @@ def read_products(keyword=None, from_price=None, to_price=None):
         return [product for product in read_products() if product["price"] >= from_price and product["price"] <= to_price]
     return products
 
-
 def read_products_by_cate_id(cate_id):
     return [product for product in read_products() if product["category_id"] == cate_id]
+
+def add_product(name, description, price, image, category):
+    products = read_products()
+    products.append({
+        "id": len(products) + 1,
+        "name": name,
+        "description": description,
+        "price": float(price),
+        "images": image,
+        "category_id": int(category)
+    })
+    return update_product_json(products)
+
+
+def update_product_json(products):
+    try:
+        f = open(os.path.join(app.root_path, "data/products.json"), "w", encoding="utf-8")
+        json.dump(products, f, ensure_ascii=False, indent=4)
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+def update_product(id, name, description, price, image, category):
+    products = read_products()
+    for pro in products:
+        if pro["id"] == int(id):
+            pro["name"] = name
+            pro["description"] = description
+            pro["price"] = price
+            pro["image"] = image
+            pro["category"] = category
+            break
+    return update_product_json(products)
+
+def read_product_by_id(id):
+    products = read_products()
+    for pro in products:
+        if(pro["id"] == id):
+            return pro
+    return None
+
+def read_categories():
+    with open(os.path.join(app.root_path, "data/categories.json"), encoding="utf-8") as f:
+        return json.load(f)
 
 if __name__ == "__main__":
     print(read_products())
--- saleapp/main.py
+++ saleapp/main.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from saleapp import app
 from saleapp import dao
 
@@ -14,9 +14,38 @@ def product_list():
 
     return render_template("product-list.html", products=dao.read_products(keyword=keyword, from_price=from_price, to_price=to_price))
 
+
 @app.route("/products/<int:category_id>")
 def product_list_by_cate(category_id):
     return render_template("product-list.html", products=dao.read_products_by_cate_id(category_id))
+
+
+@app.route("/products/add", methods=["get", "post"])
+def product_add():
+    err_msg = None
+    if request.method == "POST":
+
+        if request.args["product_id"]:
+            p = dict(request.form.copy())
+            p["id"] = request.args["product_id"]
+
+            if dao.update_product(**p):
+                return redirect(url_for('product_list'))
+            else:
+                pass
+        else:
+            if dao.add_product(**dict(request.form)):
+                return redirect(url_for('product_list'))
+            else:
+                err_msg = "Add failed!!!"
+
+    product = None
+    if request.args["product_id"]:
+        product = dao.read_product_by_id(int(request.args["product_id"]))
+
+    categories = dao.read_categories()
+    return render_template("product-add.html", categories=categories, err_msg=err_msg, product=product)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
